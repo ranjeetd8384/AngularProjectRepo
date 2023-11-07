d5359c284e0011ae49b10a13e2a7a06d82508e5f
Ranjeet Deshmukh <deshmukhranjeet831@gmail.com>
2023-11-07 19:38:43 +0530

    added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeadingComponent } from './heading/heading.component';
+import { MovieslistComponent } from './movieslist/movieslist.component';
+import { PhotosComponent } from './photos/photos.component';
+import { ChoreListComponent } from './chore-list/chore-list.component';
+import { LinksComponent } from './links/links.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadingComponent,
+    MovieslistComponent,
+    PhotosComponent,
+    ChoreListComponent,
+    LinksComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/chore-list/chore-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chore-list',
+  templateUrl: './chore-list.component.html',
+  styleUrls: ['./chore-list.component.css']
+})
+export class ChoreListComponent {
+
+}
